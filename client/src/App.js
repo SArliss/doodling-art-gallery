3ed0e461fd44942dfa2210334b3512a14989f6df
sarliss <sinaramartins@gmail.com>
2020-02-11 09:26:51 -0500

    Refactored App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,77 +1,22 @@
 import React from 'react';
 import './App.css';
-import Drawing from './Drawing.js';
-import Immutable from 'immutable';
+import DrawArea from './DrawArea.js';
 
 class App extends React.Component {
   constructor() {
     super();
-
-    this.state = {
-      lines: new Immutable.List(),
-      isDrawing: false
-    }
+    this.state = {}
   }
 
-  componentDidMount() {
-    document.addEventListener("mouseup", this.handleMouseUp);
-  }
-
-  componentWillUnmount() {
-    document.removeEventListener("mouseup", this.handleMouseUp);
-  }
-
-  handleMouseDown = (mouseEvent) => {
-    if (mouseEvent.button !== 0) {
-      return;
-    } else {
-      const point = this.relativeCoordinatesForEvent(mouseEvent);
-      this.setState(prevState => ({
-        lines: prevState.lines.push(new Immutable.List([point])),
-        isDrawing: true
-      }));
-    }
-  }
-
-  handleMouseMove = (mouseEvent) => {
-    if (!this.state.isDrawing) {
-      return;
-    } else {
-      const point = this.relativeCoordinatesForEvent(mouseEvent);
-      this.setState(prevState => ({
-        lines: prevState.lines.updateIn([prevState.lines.size - 1], line => line.push(point))
-      }));
-    }
-  }
-
-  handleMouseUp = () => {
-    this.setState({ isDrawing: false });
-  }
-
-  relativeCoordinatesForEvent(mouseEvent) {
-    const boundingRect = this.refs.drawArea.getBoundingClientRect();
-    return new Immutable.Map({
-      x: mouseEvent.clientX - boundingRect.left,
-      y: mouseEvent.clientY - boundingRect.top,
-    });
-  }
 
   render() {
-    
     return (
       <div className="App">
         <header>
           <h1>Doodling Art Gallery</h1>
         </header>
 
-        <div
-          className="drawArea"
-          ref="drawArea"
-          onMouseDown={this.handleMouseDown}
-          onMouseMove={this.handleMouseMove}
-        >
-          <Drawing lines={this.state.lines}/>
-        </div>
+        < DrawArea />
 
       </div>
     );
