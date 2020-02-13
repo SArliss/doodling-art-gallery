2730bfeeefb77b2a0e6ecaae38132b4692842846
sarliss <sinaramartins@gmail.com>
2020-02-13 14:46:35 -0500

    Doodles controller code cleanup

--- doodles_rest/app/controllers/doodles_controller.rb
+++ doodles_rest/app/controllers/doodles_controller.rb
@@ -5,23 +5,17 @@ class DoodlesController < ApplicationController
   # GET /categories/:category_id/doodles
   def index
     @doodles = current_user.doodles
-    puts "Showing all the doodles"
     json_response(@doodles)
-    #json_response(@category.doodles)
   end
 
   # GET /categories/:category_id/doodles/:id
   def show
-    puts "Showing all the doodles"
     json_response(@doodle)
   end
 
   # POST /categories/:category_id/doodles
   def create
-    puts current_user
     @category = current_user.doodles.create!(doodle_params)
-    puts @category
-    # json_response(@category, :created)
     json_response(status: "SUCCESS", message: 'doodle created successfully.')
   end
 
@@ -44,7 +38,6 @@ class DoodlesController < ApplicationController
   end
 
   def set_category
-    puts "calling set_category"
     @category = Category.find(params[:category_id])
   end
 
@@ -54,6 +47,5 @@ class DoodlesController < ApplicationController
     end
 
     @doodles = current_user.doodles
-    puts @doodles
   end
 end
