6663a03277249fc43ca970b4c3cb9233a32c284f
sarliss <sinaramartins@gmail.com>
2020-02-16 17:12:23 -0500

    Updated the react component drawing to be class based component instead of function

--- doodles_rest/client/src/Components/Drawing.js
+++ doodles_rest/client/src/Components/Drawing.js
@@ -1,25 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function Drawing({ lines }) {
-  let pathData = ""
-  lines.map((line, index) => (
-    pathData = "M " + line.map(p => (
-      `${p.get('x')} ${p.get('y')}`)).join(" L ")))
-  
-  console.log(pathData)
-  
-  return (
-    <svg className="drawing">
-      {lines.map((line, index) => (
-        <path
-          key={index}
-          className="path"
-          d={"M " + line.map(p => (
-            `${p.get('x')} ${p.get('y')}`)).join(" L ")}
-        />
-      ))}
+class Drawing extends Component {
+  constructor() {
+    super()
+    this.state = {
+      
+    }
+  }
+
+  render() {
+    return (
+      <svg className="drawing">
+        {this.props.lines.map((line, index) => (
+          <path
+            key={index}
+            className="path"
+            d={"M " + line.map(p => (
+              `${p.get('x')} ${p.get('y')}`)).join(" L ")}
+          />
+        ))}
       </svg>
-  )
+    )
+  }
 }
 
 export default Drawing;
