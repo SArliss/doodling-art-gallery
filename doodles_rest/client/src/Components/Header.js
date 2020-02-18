843b0a9f2cc83acecc6e882c9bf2f4954c06a204
sarliss <sinaramartins@gmail.com>
2020-02-18 00:57:26 -0500

    Created menu in header

--- doodles_rest/client/src/Components/Header.js
+++ doodles_rest/client/src/Components/Header.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-function Header() {
+function Header(props) {
   return (
-    <header>
-      <h1>Doodling Art Gallery</h1>
-    </header>
+    <div>
+
+      <header>
+      <Link to="/">
+        <h1>Doodling Art Gallery</h1>
+      </Link>
+      </header>
+
+      <div className="menu">
+        {props.currentUser &&
+          <ul>
+            <Link to="/"><li>Home</li></Link>
+            <Link to="/explore"><li>My Doodles</li></Link>
+            <Link to="/myprofile"><li>Public Doodles</li></Link>
+            <Link to="/login" onClick={props.handleLogout}><li>Logout</li></Link>
+          </ul >
+        }
+      </div>
+
+    </div>
   )
 }
 
