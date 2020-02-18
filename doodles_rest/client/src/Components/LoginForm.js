fbf787c5bc37c1284fa4f7495cca0a9d6d081ef5
sarliss <sinaramartins@gmail.com>
2020-02-18 00:59:53 -0500

    Created LoginForm

---
+++ doodles_rest/client/src/Components/LoginForm.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class LoginForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: ""
+    }
+  }
+
+  handleChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  }
+
+  render() {
+    return (
+      <div>
+        {this.props.currentUser ?
+          <div className="logged-in">
+            <h1>Hello! You are already logged-in. Please acess <a href="/user/doodles">your gallery.</a></h1>
+          </div>
+          :
+          <div className="login-form">
+            <form onSubmit={(e) => this.props.handleLogin(e, this.state)}>
+              <h2>Login!</h2>
+              {this.state.errorText && <p className="error-text">{this.state.errorText}</p>}
+              <label htmlFor="email">Email</label>
+              <input
+                type="text"
+                name="email"
+                value={this.state.email}
+                onChange={this.handleChange}
+              />
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                name="password"
+                value={this.state.password}
+                onChange={this.handleChange}
+              />
+              <button>Submit</button>
+              <Link to="/register">Register</Link>
+            </form>
+          </div>
+        }
+      </div>
+    )
+  }
+}
