9a7f9084c6129fb0808f4f91e5e137fc481d1071
sarliss <sinaramartins@gmail.com>
2020-02-18 11:49:44 -0500

    Updated LoginForm

--- doodles_rest/client/src/Components/LoginForm.js
+++ doodles_rest/client/src/Components/LoginForm.js
@@ -26,23 +26,28 @@ export default class LoginForm extends Component {
           :
           <div className="login-form">
             <form onSubmit={(e) => this.props.handleLogin(e, this.state)}>
-              <h2>Login!</h2>
+              <h2>Login</h2>
               {this.state.errorText && <p className="error-text">{this.state.errorText}</p>}
               <label htmlFor="email">Email</label>
+              <br></br>
               <input
                 type="text"
                 name="email"
                 value={this.state.email}
                 onChange={this.handleChange}
               />
+              <br></br>
               <label htmlFor="password">Password</label>
+              <br></br>
               <input
                 type="password"
                 name="password"
                 value={this.state.password}
                 onChange={this.handleChange}
               />
+              <br></br>
               <button>Submit</button>
+              <br></br>
               <Link to="/register">Register</Link>
             </form>
           </div>
