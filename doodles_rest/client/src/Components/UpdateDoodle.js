6c8ae5b7a2dd8911e6e5dc3f1ac83dd58f516812
sarliss <sinaramartins@gmail.com>
2020-02-19 17:49:14 -0500

    Created Doodle details component

---
+++ doodles_rest/client/src/Components/UpdateDoodle.js
@@ -0,0 +1,86 @@
+import React, { Component } from "react";
+import { updateDoodle } from "../Services/Api-helper";
+import { getOneUserDoodle } from "../Services/Api-helper";
+import { withRouter } from "react-router-dom";
+
+class UpdateDoodle extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+      path: "",
+      doodle: []
+    };
+  }
+  componentDidMount = () => {
+    this.getSingleDoodle(
+      this.props.match.params.category,
+      this.props.match.params.id
+    );
+  };
+
+  getSingleDoodle = async (categoryId, doodleId) => {
+    const doodle = await getOneUserDoodle(categoryId, doodleId);
+    this.setState({ doodle });
+    this.setState({
+      title: this.state.doodle.title,
+      path: this.state.doodle.path
+    });
+  };
+
+  handleDropdown = e => {
+    this.setState({
+      category: e.target.value
+    });
+  };
+
+  updateDoodle = async (e, category_id, id, postData) => {
+    e.preventDefault();
+    const res = await updateDoodle(category_id, id, postData);
+    this.props.history.push(`/user/doodles`);
+  };
+
+  handleChange = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    return (
+      <form
+        onSubmit={e =>
+          this.updateDoodle(
+            e,
+            this.props.match.params.category,
+            this.props.match.params.id,
+            this.state
+          )
+        }
+      >
+        <label htmlFor="title"> Title: </label>
+        <input
+          type="text"
+          name="title"
+          value={this.state.title}
+          onChange={this.handleChange}
+        />
+
+        <br></br>
+        <label htmlFor="category"> Category: </label>
+        <select onChange={this.handleDropdown}>
+          <option> Please select one</option>
+          <option name="Objects" value={1}> Objects</option>
+          <option name="Animals" value={2}> Animals</option>
+          <option name="Geometric Shapes" value={3}>Geometric Shapes</option>
+          <option name="Nature" value={4}> Nature</option>
+          <option name="Human Body" value={5}> Human Body</option>
+          <option name="Food" value={6}> Food</option>
+          <option name="Others" value={7}> Others</option>
+        </select>
+        <br></br>
+        <button>Update</button>
+      </form>
+    );
+  }
+}
+export default withRouter(UpdateDoodle);
